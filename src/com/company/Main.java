9d993f6415249f40e9f2048e9672850f1448726c
amir-feiz <103141846+amir-feiz@users.noreply.github.com>
2023-05-17 01:53:38 +0330

    coloring algorithm finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,30 @@
 package com.company;
 import com.company.File.JSonFileWriter;
 import com.company.File.JsonFileReader;
+import com.company.models.GFGGraph;
 
 public class Main {
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("\n\n\nHello algorithm project");
-        JSonFileWriter.WriteJSONExample();
-        JsonFileReader.fileReader();
+        GFGGraph g1 = new GFGGraph(5);
+        g1.addEdge(0, 1);
+        g1.addEdge(0, 2);
+        g1.addEdge(1, 2);
+        g1.addEdge(1, 3);
+        g1.addEdge(2, 3);
+        g1.addEdge(3, 4);
+        System.out.println("Coloring of graph 1");
+        g1.greedyColoring();
+
+        System.out.println();
+        GFGGraph g2 = new GFGGraph(5);
+        g2.addEdge(0, 1);
+        g2.addEdge(0, 2);
+        g2.addEdge(1, 2);
+        g2.addEdge(1, 4);
+        g2.addEdge(2, 4);
+        g2.addEdge(4, 3);
+        System.out.println("Coloring of graph 2 ");
+        g2.greedyColoring();
     }
 }
