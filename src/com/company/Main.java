3ca9be6a0ce483fcf5be7f713fed84ca5e88fe5b
mohammadkazem harandi <108237484+mohammadkahr@users.noreply.github.com>
2023-05-26 21:22:13 +0330

    Kz v.1.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,13 @@
 package com.company;
 
-import com.company.Dijkstra.Dijkstra;
-import com.company.Dijkstra.Edge;
-import com.company.Dijkstra.Graph;
-import com.company.Dijkstra.Vertex;
-import com.company.File.colorGraphFile.ReadColorGraph;
-import com.company.File.colorGraphFile.WriteColorGraph;
-import com.company.File.knapSackFile.ReadKnapsack1;
-import com.company.File.sequence.SequenceReader;
-import com.company.File.sequence.SequenceWriter;
-import com.company.models.GFGGraph;
-
+import com.company.Dijkstra.*;
+import com.company.File.colorGraphFile.*;
+import com.company.File.knapSackFile.*;
+import com.company.File.sequence.*;
+import com.company.models.*;
 import com.company.View.*;
-import com.company.models.Knapsack;
-import com.company.models.Node;
-import com.company.models.SequenceCheck;
-
 import java.io.IOException;
 import java.util.*;
-import java.util.ArrayList;
-
 import static com.company.Dijkstra.Dijkstra.*;
 import static java.lang.System.exit;
 
@@ -29,124 +17,13 @@ public class Main {
     static ArrayList<GFGGraph> gfgGraphs;
     static int[][] knapsack ;
     static Node[][] allNodes ;
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public static void main(String[] args) throws IOException {
         ReadColorGraph r = new ReadColorGraph();
-
         gfgGraphs = r.readFile();
         knapsack = ReadKnapsack1.inputFromFile();
         allNodes = SequenceReader.FileReader();
-
         SequenceWriter.RandomWriter();
-
-
         Main.mainMenu();
-
-        //Dijkstra
-        {/*
-            int numVertices = 10; // تعداد رئوس مورد نیاز
-
-            Graph graph = generateRandomGraph(numVertices);
-            Vertex source = graph.getVertices().get(0); // رأس مبدأ
-
-            PrintColor.printNorm("Randomly generated graph:");
-            PrintColor.printNorm("Vertices: " + graph.getVertices().size());
-            PrintColor.printNorm("Edges: " + graph.getEdges().size());
-
-            PrintColor.printPur("\nApplying Dijkstra's algorithm:");
-            applyDijkstra(graph, source);
-        */
-        }
-        //coloring graph test
-        {/*
-            GFGGraph g1 = new GFGGraph(5);
-            g1.addEdge(0, 1);
-            g1.addEdge(0, 2);
-            g1.addEdge(1, 2);
-            g1.addEdge(1, 3);
-            g1.addEdge(2, 3);
-            g1.addEdge(3, 4);
-            System.out.println("Coloring of graph 1");
-            g1.greedyColoring();
-
-            System.out.println();
-            GFGGraph g2 = new GFGGraph(5);
-            g2.addEdge(0, 1);
-            g2.addEdge(0, 2);
-            g2.addEdge(1, 2);
-            g2.addEdge(1, 4);
-            g2.addEdge(2, 4);
-            g2.addEdge(4, 3);
-            System.out.println("Coloring of graph 2 ");
-            g2.greedyColoring();
-        */
-        }
-        //knapsack for most value
-        {/*
-            Carpet c1 = new Carpet(60,10);
-            Carpet c2 = new Carpet(100,20);
-            Carpet c3 = new Carpet(120,30);
-            int val[] = new int[] { c1.getPrice(), c2.getPrice(), c3.getPrice() };
-            int wt[] = new int[] { c1.getWeigh(), c2.getWeigh(), c3.getWeigh() };
-            int W = 50;
-            int n = val.length;
-            System.out.println("max value is: " + knapSack(W, wt, val, n));
-            */
-        }
-        //knapsack test for  most count
-        {/*
-            Carpet c1 = new Carpet(60,10);
-            Carpet c2 = new Carpet(100,20);
-            Carpet c3 = new Carpet(120,30);
-            Carpet c4 = new Carpet(40,20);
-            Carpet c5 = new Carpet(120,30);
-            int val[] = new int[] { c1.getPrice(), c2.getPrice(), c3.getPrice() ,c4.getPrice(),c5.getPrice() };
-            int max = 200;
-            System.out.println("max value is: " + knapsackForCount(val,max));
-        */
-        }
-        //sort algorithm test
-        {/*
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
-            Node n8 = new Node(8);
-            Node n9 = new Node(9);
-            Node n10 = new Node(10);
-
-            Node mainNode[] = new Node[10];
-
-            mainNode[0] = n1;
-            mainNode[1] = n2;
-            mainNode[2] = n3;
-            mainNode[3] = n4;
-            mainNode[4] = n5;
-            mainNode[5] = n6;
-            mainNode[6] = n7;
-            mainNode[7] = n8;
-            mainNode[8] = n9;
-            mainNode[9] = n10;
-
-
-    }
-
-
-            Random random = new Random();
-            Node n[][] = new Node[10][10];
-            for (int i = 0; i < 10; i++) {
-                for (int j = 0; j < 10; j++) {
-                    n[i][j] = mainNode[random.nextInt(0, 10)];
-                    System.out.print(n[i][j].getValue() + " ");
-                }
-                System.out.println();
-            }
-            int answer[] = SequenceCheck.chooseMostSimilars(mainNode, n);
-            System.out.println(answer[0] + "  " + answer[1] + "  " + answer[2]);
-        */
-        }
     }
 
     //MainMenu();
@@ -192,7 +69,6 @@ public class Main {
             }
         }
     }
-
     static void caseOne(int n) {
         if (n == 1) {
             Graph graph = Dijkstra.generateRandomGraph(15);
@@ -302,6 +178,4 @@ public class Main {
         }
 
     }
-
-
 }
--- src/com/company/View/PrintColor.java
+++ src/com/company/View/PrintColor.java
@@ -77,6 +77,4 @@ public class PrintColor {
                 + obj
                 + ANSI_RESET);
     }
-
-
 }
